4fb2df978353a922347a3f4f36251cd6fc731d43
Fatih Gumus <fatihgumus72@gmail.com>
2023-05-19 23:07:04 +0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Fatih Gümüş on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
